46dd1fdb7797bbbca4be99ad18dce6e4b02f1d45
chonpsk <chonpskz@gmail.com>
2019-03-24 21:41:48 +0800

    accomplishment update

--- transferLadle/accomplishment.py
+++ transferLadle/accomplishment.py
@@ -10,19 +10,23 @@ def getList():
     data['state'] = '3'
     return auto_post('/1/Accomplishment/getAccomplishmentList', data, 30).json()['action']['not_received']
 
-def receive(lst):
-    acc_ids = []
-    for acc in lst:
-        if lst[acc]['complete_flg']:
-            acc_ids.append(acc)
-    if len(acc_ids) == 0:
-        return False
-    data = getData()
-    data['accomplishment_ids[]'] = acc_ids
-    try:
-        session.post('http://' + host + '/1/Accomplishment/receiveAccomplishmentRewardBundle', data = data, timeout = 30)
-    except:
-        pass
+def receive(acmp_lst_id, acc_ids, tot = 999):
+    while tot > 0:
+        acmp_lst = getList()[acmp_lst_id]
+        acc_ids_ = acc_ids.copy()
+        for acc in acc_ids_:
+            if acmp_lst[acc]['complete_flg']:
+                acc_ids.remove(acc)
+        if len(acc_ids) == 0:
+            return False
+        data = getData()
+        data['accomplishment_ids[]'] = acc_ids
+        try:
+            session.post('http://' + host + '/1/Accomplishment/receiveAccomplishmentRewardBundle', data = data, timeout = 30)
+        except:
+            pass
+        tot -= 1
+        time.sleep(0.2)
     return True
 
 def accomplishment():
@@ -31,30 +35,9 @@ def accomplishment():
     not_received = getList()
     for state in range(1, 4):
         time.sleep(0.2)
-        while receive(not_received[str(state)]):
-            not_received = getList()
-            time.sleep(0.2)
-            print ('spend ' + str((datetime.datetime.now() - init_time).seconds) + 's now')
-
-def get_accompolishment(acco_list, tot = 999):
-    print ('get accompolishment start')
-    getList()
-    while tot > 0:
-        time.sleep(0.2)
-        lst = getList()['3']
-        for acco in acco_list:
-            if not lst[acco]['complete_flg']:
-                return
-        data = getData()
-        data['accomplishment_ids[]'] = acco_list
-        time.sleep(0.2)
-        try:
-            session.post('http://' + host + '/1/Accomplishment/receiveAccomplishmentRewardBundle', data = data, timeout = 30)
-        except:
-            continue
-        tot -= 1
-    print('get finished')
+        receive(str(state), not_received[str(state)])
+        print ('spend ' + str((datetime.datetime.now() - init_time).seconds) + 's now')
 
 def get_friendPT():
     tot = cfg.getint('accomplishment', 'friendPT')
-    get_accompolishment(['40700101'], tot)
+    receive('3', ['40700101'], tot)
